129cb1081e1733f5aede3c2d7d2cd7ce27aef5cc
Mark <33119mark@gmail.com>
2019-04-10 02:32:34 -0600

    Add JavaDoc param for packet type
    Signed-off-by: Mark <33119mark@gmail.com>

--- Bridge/src/main/java/com/andavin/protocol/ProtocolManager.java
+++ Bridge/src/main/java/com/andavin/protocol/ProtocolManager.java
@@ -48,6 +48,7 @@ public class ProtocolManager implements Versioned {
      * @param packetClass The class of the {@code Packet} that
      *                    is being listened to.
      * @param listener The PacketListener to register.
+     * @param <T> The type of packet that should be listened to.
      */
     public <T> void register(Class<T> packetClass, PacketListener<T> listener) {
         this.register(packetClass, ProtocolPriority.NORMAL, listener);
@@ -62,6 +63,7 @@ public class ProtocolManager implements Versioned {
      * @param priority The {@link ProtocolPriority} the packet should
      *                 be registered under.
      * @param listener The PacketListener to register.
+     * @param <T> The type of packet that should be listened to.
      */
     public <T> void register(Class<T> packetClass, ProtocolPriority priority, PacketListener<T> listener) {
         this.listeners.computeIfAbsent(packetClass, __ -> new EnumMap<>(ProtocolPriority.class))
